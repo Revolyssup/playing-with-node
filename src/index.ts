e07a5a144253811d6766ea206081241a8f483dfb
Ashish test <ashishjaitiwari15112000@gmail.com>
2021-05-12 14:46:27 +0530

    ignored dist

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,49 @@
-import express from 'express'
-import {connectToShards,hashit,getServer,insertURL,getShortURL} from "./db/db_shard"
-const app=express();
-connectToShards();
-app.get("/:urlID",async (req,res)=>{
-    const urlID=req.params.urlID
-    console.log("Got request to decode shortened URL "+urlID)
-    if(!urlID) res.json({
-        "err:" : "Invalid urlID"
-    })
-    const server=getServer(urlID);
-    console.log("server: "+server)
-    const ans=await getShortURL(server,urlID);
-    console.log(ans.rows[0])
-    if(ans.rowCount!=0){
-        res.send({
-            "hash":urlID,
-            "server":server,
-            "url":ans.rows[0]
-        })
-    }else res.status(404);
-});
 
-app.post("/",async (req,res)=>{
-    const url=req.query.url;
-    console.log("Got request to short and store URL "+url)
-    const hash=hashit((url as string));
-    const urlID=hash.substr(0,5);
-    const server=getServer(urlID);
-    await insertURL(server,(url as string),urlID);
-    res.send({
-        "hash":urlID,
-        "server":server,
-        "url":url
-    })
-});
 
-app.listen(3000,()=>{
-    console.log("Server started");
-})
+
+
+
+
+
+
+/**For pg sharding */
+// import express from 'express'
+// import {connectToShards,hashit,getServer,insertURL,getShortURL} from "./db/db_shard"
+// const app=express();
+// connectToShards();
+// app.get("/:urlID",async (req,res)=>{
+//     const urlID=req.params.urlID
+//     console.log("Got request to decode shortened URL "+urlID)
+//     if(!urlID) res.json({
+//         "err:" : "Invalid urlID"
+//     })
+//     const server=getServer(urlID);
+//     console.log("server: "+server)
+//     const ans=await getShortURL(server,urlID);
+//     console.log(ans.rows[0])
+//     if(ans.rowCount!=0){
+//         res.send({
+//             "hash":urlID,
+//             "server":server,
+//             "url":ans.rows[0]
+//         })
+//     }else res.status(404);
+// });
+
+// app.post("/",async (req,res)=>{
+//     const url=req.query.url;
+//     console.log("Got request to short and store URL "+url)
+//     const hash=hashit((url as string));
+//     const urlID=hash.substr(0,5);
+//     const server=getServer(urlID);
+//     await insertURL(server,(url as string),urlID);
+//     res.send({
+//         "hash":urlID,
+//         "server":server,
+//         "url":url
+//     })
+// });
+
+// app.listen(3000,()=>{
+//     console.log("Server started");
+// })
